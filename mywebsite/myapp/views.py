b0f933aab2f612431db6bb9f3938b485f63e3f96
Uncle Engineer <loong.wissawakorn@gmail.com>
2023-04-22 14:54:26 +0700

    เปลี่ยนจาก httpresponse เป็น render หน้า homepage

--- mywebsite/myapp/views.py
+++ mywebsite/myapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def Homepage(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    return render(request, 'myapp/homepage.html')
